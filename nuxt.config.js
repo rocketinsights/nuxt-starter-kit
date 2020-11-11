35f79eaaea8d91df33e0573d5d0d5a39bbd69169
Brandon Aaskov <brandonaaskov@gmail.com>
2020-11-11 17:46:39 -0500

    adds firebase auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,19 @@ process.env.API_BASE_URL = API_BASE_URL
 
 export default {
   srcDir: 'src',
+
   dev: process.env.NODE_ENV !== 'production',
+
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+  },
 
   serverMiddleware: [
     '~/serverMiddleware/redirects',
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <section>
+    <nav>
+      <h3>{{user && user.displayName}}</h3>
+      <button @click.stop.prevent="signInWithRedirect">
+        Sign In With Google
+      </button>
+      <button @click.stop.prevent="signOut">
+        Sign Out
+      </button>
+    </nav>
     <h2 class="title">Disney Movies</h2>
     <ul class="movies">
       <li v-for="movie in disneyMovies" :key="movie.title">
@@ -10,16 +19,25 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions, mapState } from 'vuex'
 
 export default {
   computed: {
     ...mapGetters({
       byStudio: 'movies/byStudio'
     }),
+    ...mapState({
+      user: ({ auth }) => auth.user
+    }),
     disneyMovies () {
       return this.byStudio('Disney')
     }
+  },
+  methods: {
+    ...mapActions({
+      signInWithRedirect: 'auth/signInWithRedirect',
+      signOut: 'auth/signOut'
+    })
   }
 }
 </script>
