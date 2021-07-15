60506772df0d94be7ce1ac83bae3cbea67556fb5
Rudy Becker <rudy.becker@gmail..com>
2021-07-15 08:47:15 -0700

    first commit add navbar, add signout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   theme: {
     extend: {
       colors: {
         blue: {
-          DEFAULT: '#3A80B5',
-          50: '#f3f8fb',
-          100: '#ecf3f9',
-          300: '#aecee5',
-          200: '#d5e5f1',
-          500: '#5296c7',
-          400: '#84b4d7',
-          700: '#2a5e83',
-          600: '#3677a6',
-          900: '#204864',
-          800: '#234d6c'
+          DEFAULT: "#3A80B5",
+          50: "#f3f8fb",
+          100: "#ecf3f9",
+          300: "#aecee5",
+          200: "#d5e5f1",
+          500: "#5296c7",
+          400: "#84b4d7",
+          700: "#2a5e83",
+          600: "#3677a6",
+          900: "#204864",
+          800: "#234d6c"
         }
       }
     }
-  }
-}
+  },
+  plugins: [require("@tailwindcss/forms")]
+};
